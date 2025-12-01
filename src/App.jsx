f0ff0b8901c7f84bb64d532a7312d4d83be73203
Lizeth Mendoza <lizethmendoza@Lizeths-MacBook-Air-4.local>
2025-11-30 23:37:51 -0800

    update vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Resume from "./pages/Resume";
 import Projects from "./pages/Projects";
